27b104916cb84eb77684b4e8c43b75b9f78e184c
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-06-28 15:26:18 -0300

    #3.5 added

---
+++ Ejercicios/Ejercicio-3.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantTablas, resultado;
+            Console.Write("Ingrese cantidad de tablas: ");
+            cantTablas = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= cantTablas; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    resultado = i * j;
+                    Console.WriteLine($"{i} * {j} = {resultado}", i, j, resultado);
+                }
+                Console.Write("Pulse una Tecla:");
+                Console.ReadLine();
+            }
+            Console.Write("F I N");
+        }
+    }
+}
