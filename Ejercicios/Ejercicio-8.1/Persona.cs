bc97231d7e11abedbd9315d43fc992495b329f1d
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-09-02 22:37:33 -0300

    ex 8.1 with no Interfaces added

--- Ejercicios/Ejercicio-8.1/Persona.cs
+++ Ejercicios/Ejercicio-8.1/Persona.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace Ejercicio_8._1
 {
@@ -7,10 +8,22 @@ namespace Ejercicio_8._1
         public int ID { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
+
+        public Persona(int ID, string Nombre, string Apellido)
+        {
+
+        }
+
     }
 
     public class Empleado : Persona
     {
+        public Empleado(int ID, string Nombre, string Apellido, 
+                        string ocupacion) : base(ID, Nombre, Apellido)
+        {
+            Ocupacion = ocupacion;
+
+        }
         public string Ocupacion { get; set; }
 
         public void Trabajar()
@@ -21,12 +34,38 @@ namespace Ejercicio_8._1
 
     public class Estudiante : Persona
     {
+        public Estudiante(int ID, string Nombre, string Apellido, string carrera) : base(ID, Nombre, Apellido)
+        {
+            Carrera =carrera;
+        }
+
         public string Carrera { get; set; }
 
         public void Estudiar()
         {
             Console.WriteLine($"{ID} estudiando");
         }
+    }
+
+    public class EstudianteTrabajador : Estudiante
+    {
+        public EstudianteTrabajador(int ID, string Nombre, string Apellido, string carrera, string ocupacion) : base(ID, Nombre, Apellido, carrera)
+        {
+            Ocupacion =ocupacion;
+        }
+
+        //Tengo que duplicar esto porque no puedo heredar de estudiante y trabajador conjuntamente
+        public string Ocupacion { get; set; }
+
+        public void Trabajar()
+        {
+            Console.WriteLine($"{ID} trabajando. Ahora no estoy estudiando");
+        }
+    }
+
+    public class Curso
+    {
+        public List<Estudiante> estudiantes { get; set; }
     }
 
 }
--- Ejercicios/Ejercicio-8.1/Program.cs
+++ Ejercicios/Ejercicio-8.1/Program.cs
@@ -6,7 +6,14 @@ namespace Ejercicio_8._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var estudiante = new Estudiante(1, "Pepe", "Sanchez", "Programacion");
+            var empleado = new Empleado(2, "Hector", "Tuga", ",maratonista");
+            var estudianteTrabajador = new EstudianteTrabajador(3, "Pepe", "Sanchez", "Programacion", "Carpintero");
+
+            Curso curso =new Curso();
+
+            curso.estudiantes.Add(estudiante);
+            curso.estudiantes.Add(estudianteTrabajador);
         }
     }
 }
