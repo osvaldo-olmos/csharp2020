430caf8d190c287e4aee6f5feceac8a3d14d2233
osvaldo-olmos <osvaldo.olmos@gmail.com>
2020-09-27 13:30:19 -0300

    Ex 11.2 added

---
+++ Ejercicios/Ejercicio-11.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ejercicio_11._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, bool> EsPar = x => (x % 2) == 0;
+
+            Console.WriteLine(EsPar(2));
+        }
+    }
+}
